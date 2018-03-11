b9bec1471b05f79fc5968e21f16a81dd562d620f
Muraru Stefan <muraru.stefaan@gmail.com>
2018-03-11 12:51:48 +0200

    Create script to get search results
    Signed-off-by: Muraru Stefan <muraru.stefaan@gmail.com>

---
+++ src/Search.js
@@ -0,0 +1,55 @@
+/**
+ * The boolean operations for searching
+ */
+const Operations = Object.freeze({
+  /**
+   * @param { Array<string> } results
+   * @param { Array<string> } currentWordDocumentNames
+   * @returns { Array<string> }
+   */
+  AND:  (results, currentWordDocumentNames) =>
+    results.filter(doc => currentWordDocumentNames.includes(doc)),
+
+  /**
+   * @param { Array<string> } results
+   * @param { Array<string> } currentWordDocumentNames
+   * @returns { Array<string> }
+   */
+  OR:   (results, currentWordDocumentNames) =>
+    results.concat(currentWordDocumentNames),
+
+  /**
+   * @param { Array<string> } results
+   * @param { Array<string> } currentWordDocumentNames
+   * @returns { Array<string> }
+   */
+  NOT:  (results, currentWordDocumentNames) =>
+    results.filter(doc => !currentWordDocumentNames.includes(doc)),
+});
+
+/**
+ * Check whether the given keyword is present in the list of accepted operations
+ *
+ * @param { string } keyword
+ * @returns { boolean }
+ */
+const isSearchKeyword = function isSearchKeyword(keyword) {
+ return !!Operations[keyword.toUpperCase()];
+};
+
+/**
+ * @param { Object } reverseIndexCollection
+ * @param { string } word
+ * @returns { Array<string> }
+ */
+const getDocumentNamesForWord = function(reverseIndexCollection, word) {
+  return !!reverseIndexCollection[word] ?
+    Object.keys(reverseIndexCollection[word]) :
+    [];
+};
+
+module.exports = exports = {
+  Operations,
+  isSearchKeyword,
+  getDocumentNamesForWord,
+};
